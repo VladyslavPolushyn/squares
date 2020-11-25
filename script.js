7c9414006005c6484904e735003622d5524f074b
VladyslavPolushyn <polexodus@gmail.com>
2020-11-25 17:04:37 +0200

    Initialisation of squares in game field

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let gameField = document.querySelector('.game-field');
+const gameField = document.querySelector('.game-field');
+
+let squareSize = 40; //don't forget to change the .square styles
+
+let numberOfSquares = Math.floor(gameField.offsetWidth / squareSize) * Math.floor(gameField.offsetHeight / squareSize);
+
+for (let i = 0; i < numberOfSquares; i++) {
+	gameField.insertAdjacentHTML('afterBegin', `<div class='square invisible'></div>`);
+}
+
+let squaresArr = document.querySelectorAll('.square');
 
 
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
